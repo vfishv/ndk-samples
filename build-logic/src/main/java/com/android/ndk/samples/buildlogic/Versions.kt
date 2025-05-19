b43ff6009cda56c2620030130a56efeb4b4fb3a8
Dan Albert <danalbert@google.com>
2025-05-19 14:38:38 -0700

    Update compiler/target SDK versions to 35.

--- build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
+++ build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
@@ -3,8 +3,8 @@ package com.android.ndk.samples.buildlogic
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val COMPILE_SDK = 34
-    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 35
+    const val TARGET_SDK = 35
     const val MIN_SDK = 21
     const val NDK = "27.1.12297006" // r27b
     const val CMAKE = "3.22.1"
